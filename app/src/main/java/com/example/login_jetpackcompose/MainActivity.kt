2b4109bb29a42b7c7e68faeb43c692e782f7c146
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-17 00:33:46 +0300

    Uygulama tamamlandi

--- app/src/main/java/com/example/login_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/login_jetpackcompose/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.login_jetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.login_jetpackcompose.ui.theme.LoginJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,9 +51,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sayfa() {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        val tfKullaniciAdi = remember { mutableStateOf("") }
+        val tfSifre = remember { mutableStateOf("") }
+        Column(
+            modifier =
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Logo")
+            TextField(
 
+                value = tfKullaniciAdi.value,
+                onValueChange = { tfKullaniciAdi.value = it },
+                label = { Text("Kullanıcı Adı") },
+                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                colors = TextFieldDefaults.textFieldColors(
+
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Red,
+                )
+            )
+            TextField(
+
+                value = tfSifre.value,
+                onValueChange = { tfSifre.value = it },
+                label = { Text("Şifre") },
+                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                colors = TextFieldDefaults.textFieldColors(
+
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Red,
+                )
+            )
+            Button(
+                onClick = { Log.e("Button", "Giriş yapıldı") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.size(250.dp,50.dp)
+            ) {
+                Text(text = "Giriş Yap")
+
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
